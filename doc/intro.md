b7ea562f14b634b424c2ed8cd7450661cd29177b
Pieter Breed <pieter.breed@allangray.co.za>
2012-11-12 07:42:26 +0200

    removing unused files

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to impeaching-rod
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- src/impeaching_rod/queries.clj
+++
@@ -1,9 +0,0 @@
-(ns impeaching-rod.queries
-  (:use impeaching-rod.common))
-
-(defrecord Query [zef]
-  clojure.lang.IFn
-  (invoke [_] (println zef))
-  (invoke [_ _] (println "oeu"))
-  (applyTo [this args] (clojure.lang.AFn/applyToHelper this args)))
-    
